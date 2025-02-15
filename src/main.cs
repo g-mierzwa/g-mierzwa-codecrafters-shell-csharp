4c57db3cd2c459bc6ab7088953685e15a7529bae
Grzegorz Mierzwa <grzesiek.mierzwa@gmail.com>
2025-02-15 13:27:45 +0100

    Added exit command

--- src/main.cs
+++ src/main.cs
@@ -4,6 +4,22 @@ using System.Net.Sockets;
 while(true)
 {
     Console.Write("$ ");
-    string? command = Console.ReadLine();
-    Console.WriteLine($"{command}: command not found");
+    var command = Console.ReadLine();
+    var tokens = command.Split(' ');
+
+    if (tokens[0] == "exit")
+    {
+        if (int.TryParse(tokens[1], out int exitCode))
+        {
+            Environment.Exit(exitCode);
+        }
+        else
+        {
+            Console.WriteLine("Wrong argument for exit command");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"{tokens[0]}: command not found");
+    }
 }
