d05cd5438632695fb048e1e2c12ea935561fc8e6
Grzegorz Mierzwa <grzesiek.mierzwa@gmail.com>
2025-02-15 13:10:40 +0100

    REPL implemented

--- src/main.cs
+++ src/main.cs
@@ -1,9 +1,9 @@
 using System.Net;
 using System.Net.Sockets;
 
-// Uncomment this line to pass the first stage
-Console.Write("$ ");
-
-// Wait for user input
-string? command = Console.ReadLine();
-Console.WriteLine($"{command}: command not found");
+while(true)
+{
+    Console.Write("$ ");
+    string? command = Console.ReadLine();
+    Console.WriteLine($"{command}: command not found");
+}
