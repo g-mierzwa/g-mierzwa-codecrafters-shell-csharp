6b11881c7ef5efce892860ac23a53439e945fc76
Grzegorz Mierzwa <grzesiek.mierzwa@gmail.com>
2025-02-15 13:06:06 +0100

    Invalid commands handling - all commands treated as invalid

--- src/main.cs
+++ src/main.cs
@@ -6,4 +6,4 @@ Console.Write("$ ");
 
 // Wait for user input
 string? command = Console.ReadLine();
-Console.WriteLine($"{command} : command not found");
+Console.WriteLine($"{command}: command not found");
