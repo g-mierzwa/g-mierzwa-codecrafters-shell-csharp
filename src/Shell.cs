f8dc036934ea6071f465eab031abfddff7509e7f
Grzegorz Mierzwa <grzesiek.mierzwa@gmail.com>
2025-02-22 19:13:02 +0100

    Fixed partial autocompletion

--- src/Shell.cs
+++ src/Shell.cs
@@ -293,6 +293,7 @@ public class Shell
             else
             {
                 full = false;
+                Console.Write("\a");
                 return commonPrefix;
             }
         }
